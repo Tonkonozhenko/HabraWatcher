de52ac6e949650d44fc7909ee3ec857036d06883
Alex Tonkonozhenko <tonkonozhenko@gmail.com>
2014-12-26 03:06:31 +0200

    Script v1.0 & README

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 HabraWatcher
 ============
-
 Script for watching to your carma/rating changes
+
+Usage
+-----
+* Go to page `http://habrahabr.ru/users/$yourHabraUsername$/topics/`
+* Open browser console
+  * Firefox: Ctrl+Shift+K
+  * Chrome: Ctrl+Shift+J
+* Paste content from `script.min.js`
+* Wait for karma changes
---
+++ script.js
@@ -0,0 +1,34 @@
+var rating_selector = '.rating .num',
+    karma_selector = '.score .num',
+    comments_selector = '.posts .post .comments .all',
+    rating = parseFloat($(rating_selector).html().replace(',', '.')),
+    karma = parseFloat($('.score .num').html().replace(',', '.')),
+    lastPostCommentsCount = parseInt($(comments_selector).first().html()),
+    delay = 1 * 000, // 1 second
+    username = document.location.pathname.split('/')[2]; // Get username from current URL
+
+setInterval(function () {
+  $.ajax({
+    method: 'GET',
+    url: 'http://habrahabr.ru/users/' + username + '/topics/',
+    success: function (data) {
+      var body = $(data),
+          newRating = parseFloat(body.find(rating_selector).html().replace(',', '.')),
+          newKarma = parseFloat(body.find(karma_selector).html().replace(',', '.')),
+          newCommentsCount = parseInt(body.find(comments_selector).first().html());
+
+      if (newRating != rating) {
+        alert("New rating: " + newRating + ', change: ' + (newRating - rating));
+        rating = newRating;
+      }
+      if (newKarma != karma) {
+        alert("New carma: " + newKarma + ', change: ' + (newKarma - karma));
+        karma = newKarma;
+      }
+      if (newCommentsCount != lastPostCommentsCount) {
+        alert("Comments added: " + (newCommentsCount - lastPostCommentsCount));
+        lastPostCommentsCount = newCommentsCount;
+      }
+    }
+  })
+}, delay);
